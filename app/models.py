931f5c88a41ab57e990be6a1b651ee32b7cb8e62
Nits2097 <nits2097@gmail.com>
2018-02-13 14:44:38 +0530

    done

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 ############################################################
-from vote.models import VoteModel
+#from vote.models import VoteModel
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
